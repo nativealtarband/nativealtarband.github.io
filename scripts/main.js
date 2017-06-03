c9fc7651aa060c963fa76aec109de110f1b4491c
Jimmy Pocock <jimmypocock@yahoo.com>
2017-06-03 16:24:31 -0500

    finished styling. can read csv for tour dates

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   function fullscreen(){
     $('.bx-wrapper, .bx-viewport, .bxslider, .background').css({
-      height: ($(window).height() - $('.navbar').height())
+      height: $(window).height()
     });
   }
 
@@ -20,4 +20,36 @@ $(document).ready(function(){
   $(window).resize(function() {
     fullscreen();
   });
+
+  function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    var $table = $('#tour-dates table tbody');
+    if (allTextLines.length > 1) {
+      for (var i = 1; i < allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        addRow($table, data);
+      }
+    } else {
+      addRow($table, ['TBA', 'TBA']);
+    }
+  }
+
+  function addRow(table, data) {
+    var $tr = $('<tr>');
+    $tr.append($('<td>').text(data[0]));
+    $tr.append($('<td>').text(data[1]));
+    table.append($tr);
+  }
+
+  $.ajax({
+    type: "GET",
+    url: "../tour-dates.csv",
+    dataType: "text",
+    success: function(data) { processData(data); },
+    error: function(err) { console.log(err); }
+  });
+
 });
